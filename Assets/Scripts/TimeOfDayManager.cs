b24f302d9667dfeaba179df553146ebf005ca2db
Nuutti <snuutti@protonmail.com>
2025-02-10 22:26:10 +0200

    Sun rotates according to time of day

--- Assets/Scripts/TimeOfDayManager.cs
+++ Assets/Scripts/TimeOfDayManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class TimeOfDayManager : MonoBehaviour
 {
+    [SerializeField] private Light sun;
     [SerializeField] private float timeScale = 1;
     
     public int Day { get; private set; } = 1;
@@ -15,24 +16,31 @@ public class TimeOfDayManager : MonoBehaviour
     private void Update()
     {
         _time += Time.deltaTime * timeScale;
-        if (!(_time >= 1))
+        if (_time >= 1)
         {
-            return;
+            Minute++;
+            _time = 0;
+            
+            if (Minute >= 60)
+            {
+                Hour++;
+                Minute = 0;
+            }
+            
+            if (Hour >= 24)
+            {
+                Day++;
+                Hour = 0;
+            }
         }
-        
-        Minute++;
-        _time = 0;
-        
-        if (Minute >= 60)
-        {
-            Hour++;
-            Minute = 0;
-        }
-        
-        if (Hour >= 24)
-        {
-            Day++;
-            Hour = 0;
-        }
+
+        UpdateSunRotation();
+    }
+
+    private void UpdateSunRotation()
+    {
+        var timeInDay = Hour + Minute / 60f;
+        var sunAngle = timeInDay / 24f * 360f;
+        sun.transform.rotation = Quaternion.Euler(sunAngle - 90f, 170f, 0f);
     }
 }
